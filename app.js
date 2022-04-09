56f1cbbbb86293e7dbe221d4d4793bf2c156bfd5
BrianCodeNha <nmchi1990@gmail.com>
2022-04-09 16:41:55 +0700

    send response

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const http = require('http')
+// goi globa module http
+
+const http = require("http");
+
+// creater server from http, có tham số là một function (resquest, response)
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.headers, req.method)
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First App</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(3000);
+server.listen(3000)
