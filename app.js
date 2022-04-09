1f47d513dc3b0f3da46fa613af999f709c8bc80a
BrianCodeNha <nmchi1990@gmail.com>
2022-04-09 17:34:41 +0700

    chuyen huong request 2

--- app.js
+++ app.js
@@ -2,17 +2,26 @@
 
 const http = require("http");
 
+const fs = require('fs')
+
 // creater server from http, có tham số là một function (resquest, response)
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
   res.write("<html>");
   res.write("<head><title>Enter Message</title></head>");
-  res.write("<body><form action='/mesage' method='POST'><input type='text' name='message' /><button>send</button></form></body>");
+  res.write("<body><form action='/message' method='POST' ><input type='text' name='message' /><button>send</button></form></body>");
   res.write("</html>");
   return res.end();
     }
-//   res.setHeader("Content-Type", "text/html"); // không dùng header vẫn load ra được trang html!!!!!
+
+    if(req.url === '/message' && req.method ==='POST'){
+        fs.writeFileSync('message.txt','DUMMY')
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end();
+          }
+  res.setHeader("Content-Type", "text/html"); // không dùng header vẫn load ra được trang html!!!!!
   res.write("<html>");
   res.write("<head><title>My First App</title></head>");
   res.write("<body><h1>Hello from my Node.js Server</h1></body>");
