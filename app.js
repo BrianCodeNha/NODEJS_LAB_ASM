44758261e65767697f77d13b7535ff028d2b8762
BrianCodeNha <nmchi1990@gmail.com>
2022-04-09 23:55:55 +0700

    module.exports ...

--- app.js
+++ app.js
@@ -4,40 +4,12 @@ const http = require("http");
 
 const fs = require('fs')
 
+const routes = require('./routes')
+
+console.log(routes.someText)
+
 // creater server from http, có tham số là một function (resquest, response)
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-  res.write("<html>");
-  res.write("<head><title>Enter Message</title></head>");
-  res.write("<body><form action='/message' method='POST' ><input type='text' name='message' /><button>send</button></form></body>");
-  res.write("</html>");
-  return res.end();
-    }
-
-    if(req.url === '/message' && req.method ==='POST'){
-        const body = [];
-        req.on('data',(chunk) => {
-            body.push(chunk);
-            
-        })
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1]
-            fs.writeFileSync('message.txt',message)
-            
-        })     
-
-        res.statusCode = 302;
-        res.setHeader('Location','/')
-        return res.end();
-          }
-  res.setHeader("Content-Type", "text/html"); // không dùng header vẫn load ra được trang html!!!!!
-  res.write("<html>");
-  res.write("<head><title>My First App</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000)
---
+++ routes.js
@@ -0,0 +1,49 @@
+
+const fs = require('fs');
+
+const handlerFetch = (req, res) => {
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST' ><input type='text' name='message' /><button>send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      }); //sync là dạng đồng bộ dẫn đến code block, do đó không dùng mà chuyển sang dạng bất động bộ có nghĩa là nodejs yêu cầu sever làm gì đó và để đó, sau đó thực hiện các request khác cuối cùng thì nó mới quay lại thực hiện, chứ không phải đi tuần tự
+    });
+  }
+  res.setHeader("Content-Type", "text/html"); // không dùng header vẫn load ra được trang html!!!!!
+  res.write("<html>");
+  res.write("<head><title>My First App</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+// module.exports = handlerFetch;
+
+// module.exports = {
+//     handler: handlerFetch,
+//     someText: 'some hard code'
+// }
+
+// module.exports.handler = handlerFetch;
+// module.exports.someText = 'some hard code'
+
+exports.handler = handlerFetch;
+exports.someText = 'some hard code';
