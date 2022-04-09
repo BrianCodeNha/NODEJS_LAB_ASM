5708d601895989c8423389b8b437943a24c733d6
BrianCodeNha <nmchi1990@gmail.com>
2022-04-09 17:11:05 +0700

    chuyen huong request

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const http = require("http");
 // creater server from http, có tham số là một function (resquest, response)
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.headers, req.method)
-  res.setHeader("Content-Type", "text/html");
+    if(req.url === '/'){
+  res.write("<html>");
+  res.write("<head><title>Enter Message</title></head>");
+  res.write("<body><form action='/mesage' method='POST'><input type='text' name='message' /><button>send</button></form></body>");
+  res.write("</html>");
+  return res.end();
+    }
+//   res.setHeader("Content-Type", "text/html"); // không dùng header vẫn load ra được trang html!!!!!
   res.write("<html>");
   res.write("<head><title>My First App</title></head>");
   res.write("<body><h1>Hello from my Node.js Server</h1></body>");
