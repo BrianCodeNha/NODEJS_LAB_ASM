6edcf33945388c92dae4e990ed7c0397b56f0e41
BrianCodeNha <nmchi1990@gmail.com>
2022-04-11 12:02:45 +0700

    make route

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-// goi globa module http
-
-const http = require("http");
-
 // SETUP expressjs
 
 const express = require('express')
 
 const app = express();
 
-// create middleware
+// create middleware 
 
-app.use((req, res, next) => {
-    console.log('this is a middleware');    
+app.use('/',(req, res, next) => {
+    console.log('this is always run');     
     next();
 })
 
-app.use((req, res, next) => {
-    console.log('this is the second middleware');
+app.use('/add-product',(req, res, next) => {
+    console.log('this is a middleware');
+    res.send('<h1>The Add product page</h1>')   
+})
+
+app.use('/',(req, res, next) => {
+    console.log('this is the second middleware'); 
     res.send('<h1>Hello from Express.js</h1>')
 })
 
 
+
 // creater server from http, có tham số là một function (resquest, response)
 
-const server = http.createServer(app);
-
-server.listen(3000)
+app.listen(3000)
