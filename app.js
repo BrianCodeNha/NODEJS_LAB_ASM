92d807611cd569c00d55d6ac343bcf74a25e8d44
BrianCodeNha <nmchi1990@gmail.com>
2022-04-13 14:27:48 +0700

    them loi 404

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const bodyParser = require('body-Parser');
 app.use(bodyParser.urlencoded({extends: false}))
 
 app.use(adminRouter);
-
 app.use(shopRouter)
 
-
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 // creater server from http, có tham số là một function (resquest, response)
 
