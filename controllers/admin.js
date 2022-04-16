a0f94017a37d936e3cc7fe29713fb9e4057c50cd
BrianCodeNha <nmchi1990@gmail.com>
2022-04-16 20:54:41 +0700

    edit data

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,10 +13,21 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDescription = req.body.description
+  const updatedPrice = req.body.price
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice)
+  updatedProduct.save();
+  res.redirect('/admin/products')
+}
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
