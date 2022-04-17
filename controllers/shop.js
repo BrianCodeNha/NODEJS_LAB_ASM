cd6975ca1fb0dbd7d42c110dbc763e5bf886d77c
BrianCodeNha <nmchi1990@gmail.com>
2022-04-17 23:11:06 +0700

    findbyId and findAll with where in sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,10 +16,21 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  // Product.findAll({ where: { id: prodId } })
+  //   .then((products) => {
+  //     res.render("shop/product-detail", {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
   Product.findById(prodId)
-    .then(([product]) => {
+    .then(product => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
@@ -36,7 +47,7 @@ exports.getIndex = (req, res, next) => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
-        path: "/products",
+        path: "/",
       });
     })
     .catch((err) => console.error(err));
