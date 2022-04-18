91352745704ae062c7bb46cf5809167cd4a7a45a
BrianCodeNha <nmchi1990@gmail.com>
2022-04-18 21:20:43 +0700

    install mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '097359', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://BrianNguyen:097359@cluster0.c8rh7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("connected");
+      callback(client);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-module.exports = sequelize;
+
+module.exports = mongoConnect;
