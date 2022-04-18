d3e5009e0e04b033439cf74a4d78e2258aba03fb
BrianCodeNha <nmchi1990@gmail.com>
2022-04-18 09:31:18 +0700

    add relation: product belong to user

--- models/user.js
+++ models/user.js
@@ -2,12 +2,16 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
-const User = Sequelize.define("user", {
+const User = sequelize.define("user", {
   id: {
       type: Sequelize.INTEGER,
       primaryKey: true,
       allowNull: false,
       autoIncrement: true
   },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
 
 });
+
+module.exports = User;
