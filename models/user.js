a0ff9d1738f74aaa4fff02025b53f8223a9e88af
BrianCodeNha <nmchi1990@gmail.com>
2022-04-19 20:51:02 +0700

    display orders to view

--- models/user.js
+++ models/user.js
@@ -45,28 +45,31 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return this.getCart().then(products => {
-      const order = {
-        item: products,
-        user: {
-          _id: new mongodb.ObjectId(this._id),
-          name: this.name
-        }
-      }
-      return db
-        .collection("orders")
-        .insertOne(order);
-    })
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          item: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { item: [] };
         return db
-        .collection("users")
-        .updateOne(
-          { _id: new mongodb.ObjectId(this._id) },
-          { $set: { cart:{ item: [] } } },
-        );
+          .collection("users")
+          .updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: { item: [] } } }
+          );
       });
-      
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db.collection("orders").find({ "user._id": this._id }).toArray();
   }
 
   getCart() {
