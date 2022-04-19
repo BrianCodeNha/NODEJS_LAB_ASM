2f38eb7a037a7469ea57f1bcb0571c5835505ec9
BrianCodeNha <nmchi1990@gmail.com>
2022-04-19 15:37:04 +0700

    finish add to cart function

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findUserById("625e444b1587ccd88a316167")
     .then(user => {
-      req.user = user;
+      req.user =   new User(user.name, user.email, user.cart, user._id);
       next(); // chỉ để next ở đây, để bên dưới sẽ bị lỗi undefined user
     })
     .catch(err => console.log(err))
--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(name, email, cart) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
     this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -14,7 +15,10 @@ class User {
   }
 
   addToCart (product) {
-    const cartProduct = this.cart.items.findIndex(cp => cp._id === product._id);
+    // const cartProduct = this.cart.items.findIndex(cp => cp._id === product._id);
+    const updatedCart = {item: [{productId: product._id, quantity: 1}]};
+    const db = getDb();
+    return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: {cart: updatedCart}})
   }
 
   static findUserById(id) {
