3b300e0c49cf4557f417ac718b8c92cbbdb206e0
BrianCodeNha <nmchi1990@gmail.com>
2022-04-20 12:40:24 +0700

    setup co ban hoan thanh

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
     res.send('<h1>Welcome!</h1>');
